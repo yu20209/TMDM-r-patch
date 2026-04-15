cd008b4b149a2060df0ad885aa1439e32cbce732
Yuxuan <2329952783@qq.com>
2026-04-15 15:47:05 +0800

    Update runner9_NS_transformer.py

--- TMDM/runner9_NS_transformer.py
+++ TMDM/runner9_NS_transformer.py
@@ -71,7 +71,9 @@ if __name__ == '__main__':
     parser.add_argument('--loss', type=str, default='mse', help='loss function')
     parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
     parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
-
+    #
+    parser.add_argument('--use_residual_tmdm', type=int, default=1)
+    parser.add_argument('--lambda_prior', type=float, default=0.1)
     # GPU
     parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
     parser.add_argument('--gpu', type=int, default=0, help='gpu')
